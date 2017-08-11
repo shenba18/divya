85e7bdd71985e10e4b136cb02e810096c41ddcc9
divya <divya18091996mnm@gmail.com>
2017-08-11 16:31:13 +0530

    Create div.c

---
+++ div.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+char n;
+scanf("%c",&n);
+if((n=='a')||(n=='e')||(n=='i')||(n=='o')||(n=='u'))
+printf("%c is a vowel",n);
+else
+printf("%c is not a vowel",n);
+}
