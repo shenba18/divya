f6ce1248619b9c14462635ff5c8124b58f70fdac
divya <divya18091996mnm@gmail.com>
2017-08-13 13:03:23 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,16 @@
+#include <stdio.h>  
+ 
+int main()
+{  
+    int counter; 
+    printf("Even numbers between 1 to 100\n");
+    for(counter = 1; counter <= 100; counter++)
+    {  
+        if(counter%2 == 0)
+        { 
+            printf("%d ", counter);  
+        }  
+    }  
+ 
+} 
+ 
