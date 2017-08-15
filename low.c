bfee0a5a753e612ccae91936814c7aa85f96d1fc
divya <divya18091996mnm@gmail.com>
2017-08-15 10:09:15 +0530

    Create low.c

---
+++ low.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+	int val[5],h,l,i;
+	for(i=0;i<5;i++)
+        {
+		printf("\nENTER VALUE-%d: ",i+1);
+		scanf("%d",&val[i]);
+	}
+	l=val[0];
+	h=val[0];
+	for(i=0;i<5;i++)
+        {
+		if(val[i]>h)
+			h=val[i];
+		else
+                {
+			if(val[i]<l)
+				l=val[i];
+		}
+	}
+	printf("\nHIGHEST VALUE IS %d",h);
+	printf("\nLOWEST VALUE IS %d",l);
+	}
